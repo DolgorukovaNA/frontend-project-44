c380e307defb312aabc39a8a4e4d736f38036487
ND <dolgorukovva@gmail.com>
2024-12-17 14:23:19 +0300

    part 5

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
 import {getName} from "../bin/src/cli.js";
-
 console.log('Welcome to the Brain Games!');
 const name = getName();
 console.log(`Hello,${name}!`);
+console.log ('Answer "yes" if the number is even, otherwise answer "no".');
